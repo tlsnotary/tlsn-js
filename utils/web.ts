4cebeea9375723ee8b31cea667ff1ff3d301204c
tsukino <0xtsukino@gmail.com>
2024-01-08 18:08:08 -0400

    new build for error catching

--- utils/web.ts
+++ utils/web.ts
@@ -1,18 +1,25 @@
 import { prove, verify } from '../src';
 
 (async function runTest() {
-  console.time('prove');
-  const proof = await prove('https://swapi.dev/api/people/1', {
-    method: 'GET',
-    maxTranscriptSize: 16384,
-    notaryUrl: 'http://localhost:7047',
-    websocketProxyUrl: 'ws://notary.efprivacyscaling.org:55688?token=swapi.dev',
-  });
-  console.timeEnd('prove');
+  try {
+    console.log('hihihihihihihih')
+    console.time('prove');
+    const proof = await prove('https://swapi.dev/api/people/1', {
+      method: 'GET',
+      maxTranscriptSize: 16384,
+      notaryUrl: 'http://localhost:7047',
+      websocketProxyUrl: 'ws://localhost:55688',
+    });
+    console.timeEnd('prove');
 
-  console.time('verify');
-  const result = await verify(proof);
-  console.timeEnd('verify');
+    console.time('verify');
+    const result = await verify(proof);
+    console.timeEnd('verify');
 
-  console.log(result);
+    console.log(result);
+  } catch(err) {
+    console.log('caught error from wasm');
+    console.error(err);
+  }
+
 })();
