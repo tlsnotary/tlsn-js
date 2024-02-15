829b59df81014f6ba59b398cdf7314fe61a0ffca
tsukino <87639218+0xtsukino@users.noreply.github.com>
2024-02-15 05:52:50 -0500

    chore: add react/typescript demo and quick start (#14)
    * chore: add react/typescript demo and quick start
    
    # Conflicts:
    #	package.json
    
    * doc: improve quickstart
    
    ---------
    
    Co-authored-by: Hendrik Eeckhaut <hendrik@eeckhaut.org>

---
+++ demo/react-ts-webpack/webpack.js
@@ -0,0 +1,100 @@
+var webpack = require('webpack'),
+  path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ASSET_PATH = process.env.ASSET_PATH || '/';
+
+var alias = {};
+
+var fileExtensions = [
+  'jpg',
+  'jpeg',
+  'png',
+  'gif',
+  'eot',
+  'otf',
+  'svg',
+  'ttf',
+  'woff',
+  'woff2',
+];
+
+var options = {
+  ignoreWarnings: [
+    /Circular dependency between chunks with runtime/,
+    /ResizeObserver loop completed with undelivered notifications/,
+  ],
+  mode: 'development',
+  entry: {
+    app: path.join(__dirname, 'app.tsx'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+    publicPath: ASSET_PATH,
+  },
+  module: {
+    rules: [
+      {
+        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+        type: 'asset/resource',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('ts-loader'),
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'source-map-loader',
+          },
+          {
+            loader: require.resolve('babel-loader'),
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    alias: alias,
+    extensions: fileExtensions
+      .map((extension) => '.' + extension)
+      .concat(['.js', '.jsx', '.ts', '.tsx', '.css']),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.ejs'),
+      filename: 'index.html',
+      cache: false,
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ].filter(Boolean),
+  // Required by wasm-bindgen-rayon, in order to use SharedArrayBuffer on the Web
+  // Ref:
+  //  - https://github.com/GoogleChromeLabs/wasm-bindgen-rayon#setting-up
+  //  - https://web.dev/i18n/en/coop-coep/
+  devServer: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+  },
+};
+
+module.exports = options;
---
+++ quickstart.md
@@ -0,0 +1,62 @@
+# Quick Start Guide
+
+There is a simple react/typescript demo app in `./demo/react-ts-webpack`. The directory contains a webpack configuration file that allows you to quickly bootstrap a webpack app using tlsn-js. 
+
+## Run a local notary server and websocket proxy
+
+### Websocket Proxy <a name="proxy"></a>
+
+Since a web browser doesn't have the ability to make TCP connection, we need to use a websocket proxy server.
+
+To run your own websockify proxy **locally**, run:
+```sh
+git clone https://github.com/novnc/websockify && cd websockify
+./docker/build.sh
+docker run -it --rm -p 55688:80 novnc/websockify 80 swapi.dev:443
+```
+
+Note the `swapi.dev:443` argument on the last line, this is the server we will use in this quick start.
+
+### Run a Local Notary Server <a name="local-notary"></a>
+
+For this demo, we also need to run a local notary server.
+
+1. Clone the TLSNotary repository:
+   ```shell
+   git clone https://github.com/tlsnotary/tlsn.git --branch "v0.1.0-alpha.4"
+   ```
+2. Edit the notary server config file (`notary-server/config/config.yaml`) to turn off TLS so that the browser extension can connect to the local notary server without requiring extra steps to accept self-signed certificates in the browser.
+   ```yaml
+   tls:
+      enabled: false
+   ```
+3. Run the notary server:
+   ```shell
+   cd notary-server
+   cargo run --release
+   ```
+
+The notary server will now be running in the background waiting for connections.
+
+## `tlsn-js` in a React/Typescript app
+
+### Run the
+1. Clone the repository
+    ```sh
+    git clone https://github.com/tlsnotary/tlsn-js
+    ```
+2. Go to the demo folder
+    ```sh
+    cd ./tlsn-js/demo/react-ts-webpack
+    ```
+3. Install dependencies
+    ```sh
+    npm i
+    ```
+4. Start Webpack Dev Server:
+    ```
+    npm run dev
+    ```
+5. Open `http://localhost:8080` in your browser
+6. Click the **start demo** button
+7. Open developer tools and monitor the console logs
