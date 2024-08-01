4d9f3ea6ae498b8ea21c3f3c4625feae782dabf2
tsukino <0xtsukino@gmail.com>
2024-08-01 18:20:07 +0800

    parse json from transcript

--- src/tlsn.ts
+++ src/tlsn.ts
@@ -4,9 +4,8 @@ import init, {
   Prover,
   Method,
 } from '../wasm/pkg/tlsn_wasm';
+import { processTranscript } from './utils';
 
-// const json = `{"name":"Luke Skywalker","height":"172","mass":"77","hair_color":"blond","skin_color":"fair","eye_color":"blue","birth_year":"19BBY","gender":"male","homeworld":"https://swapi.dev/api/planets/1/","films":["https://swapi.dev/api/films/1/","https://swapi.dev/api/films/2/","https://swapi.dev/api/films/3/","https://swapi.dev/api/films/6/"],"species":[],"vehicles":["https://swapi.dev/api/vehicles/14/","https://swapi.dev/api/vehicles/30/"],"starships":["https://swapi.dev/api/starships/12/","https://swapi.dev/api/starships/22/"],"created":"2014-12-09T13:50:51.644000Z","edited":"2014-12-20T21:17:56.891000Z","url":"https://swapi.dev/api/people/1/"}`;
-const json = `{"a":{"b":"c"}}`;
 export const DEFAULT_LOGGING_FILTER: string = 'info,tlsn_extension_rs=debug';
 
 export default class TLSN {
@@ -64,161 +63,47 @@ export default class TLSN {
       secretResps?: string[];
     },
   ) {
-    // await this.waitForStart();
-    //
-    // const prover = new Prover({
-    //   id: 'test',
-    //   server_dns: new URL(url).hostname || '',
-    //   max_recv_data: options?.maxRecvData,
-    //   max_sent_data: options?.maxSentData,
-    // });
-    // console.log('worker', url, {
-    //   ...options,
-    //   notaryUrl: options?.notaryUrl,
-    //   websocketProxyUrl: options?.websocketProxyUrl,
-    // });
-    // await prover.setup(options!.notaryUrl!);
-    // console.log(Buffer.from('swapi.dev').toJSON().data);
-    // await prover.send_request(options!.websocketProxyUrl!, {
-    //   uri: url,
-    //   method: options?.method || 'GET',
-    //   // @ts-ignore
-    //   headers: new Map([
-    //     ['Host', Buffer.from('swapi.dev').toJSON().data],
-    //     ['Content-Type', Buffer.from('application/json').toJSON().data],
-    //   ]),
-    //   // [['Host', Buffer.from('swapi.dev').values()]]
-    //   body: undefined,
-    // });
-    //
-    // const transcript = prover.transcript();
-    // console.log({ transcript });
-    // console.log(Buffer.from(transcript.recv).toString());
-    // console.log(Buffer.from(transcript.sent).toString());
-    //
-    // const recv = Buffer.from(transcript.recv).toString();
-    //
-    // const commitments: [string, number, number][] = [];
-    // const ret: any = {};
-    // let text = '',
-    //   ptr = -1;
-    // for (let i = 0; i < recv.length; i++) {
-    //   const char = recv.charAt(i);
-    //
-    //   if (char === '\r') {
-    //     if (!text) continue;
-    //     if (!isNaN(Number(text))) continue;
-    //
-    //     try {
-    //       const json = JSON.parse(text);
-    //       console.log({ json });
-    //     } catch (e) {
-    //       commitments.push([text, ptr, i]);
-    //     }
-    //     continue;
-    //   }
-    //
-    //   if (char === '\n') {
-    //     text = '';
-    //     ptr = -1;
-    //     continue;
-    //   }
-    //
-    //   if (ptr === -1) {
-    //     ptr = i;
-    //   }
-    //
-    //   text = text + char;
-    // }
-    //
-    // console.log(
-    //   commitments.map((data) => {
-    //     return [...data, recv.slice(data[1], data[2])];
-    //   }),
-    //   ret,
-    // );
+    await this.waitForStart();
 
-    let ptr = -1,
-      data = '';
-    const stack = [];
-    const keys = [];
-    const values: any = [];
-    console.log(json);
-    for (let i = 0; i < json.length; i++) {
-      const char = json.charAt(i);
-      const lastStack = stack[stack.length - 1];
+    const prover = new Prover({
+      id: 'test',
+      server_dns: new URL(url).hostname || '',
+      max_recv_data: options?.maxRecvData,
+      max_sent_data: options?.maxSentData,
+    });
 
-      if (lastStack === '{') {
-        expect(
-          char === '"' || char === ':' || char === ',' || char === '}',
-          `unexpected ${char}`,
-        );
-      }
+    await prover.setup(options!.notaryUrl!);
+    await prover.send_request(options!.websocketProxyUrl!, {
+      uri: url,
+      method: options?.method || 'GET',
+      // @ts-ignore
+      headers: new Map([
+        ['Host', Buffer.from('swapi.dev').toJSON().data],
+        ['Content-Type', Buffer.from('application/json').toJSON().data],
+      ]),
+      body: { a: 'apple', b: 'boy' },
+    });
 
-      if (lastStack === ':') {
-        expect(
-          char === '"' || char === '[' || /^\d$/.test(char) || char === '{',
-          `unexpected ${char}`,
-        );
-        if (char === '{') {
-          data = data + char;
-          continue;
-        }
-        stack.pop();
-      }
+    const transcript = prover.transcript();
 
-      if (lastStack === '"') {
-        if (char === '"') {
-          stack.pop();
-          continue;
-        }
-        data = data + char;
-        continue;
-      }
+    const recv = Buffer.from(transcript.recv).toString();
+    const sent = Buffer.from(transcript.sent).toString();
+    console.log('recv: ', recv);
+    console.log('sent: ', sent);
 
-      if (lastStack === '[') {
-        if (char === ']') {
-          stack.pop();
-          continue;
-        }
-      }
+    const recvCommits = processTranscript(recv);
+    const sentCommits = processTranscript(sent);
 
-      if (char === '[') {
-        stack.push(char);
-        continue;
-      }
-
-      if (char === '{') {
-        stack.push(char);
-        continue;
-      }
-
-      if (char === '"') {
-        if (ptr === -1) ptr = i;
-        stack.push(char);
-        continue;
-      }
-
-      if (char === ':') {
-        stack.push(char);
-        keys.push(data);
-        data = '';
-        continue;
-      }
-
-      if (char === '}' || (char === ',' && lastStack !== '[')) {
-        values.push([keys.join('.'), data, ptr, i]);
-        keys.pop();
-        data = '';
-        ptr = -1;
-        continue;
-      }
-    }
-
-    console.log(keys, data);
-    console.log(values);
     console.log(
-      Object.values(values).map((val: any) => json.slice(val[2], val[3])),
+      recvCommits.map(({ name, path, start, end }) => {
+        return [name || path, recv.slice(start, end), start, end];
+      }),
+    );
+
+    console.log(
+      sentCommits.map(({ name, path, start, end }) => {
+        return [name || path, sent.slice(start, end), start, end];
+      }),
     );
 
     // const commit = {
---
+++ src/utils.ts
@@ -0,0 +1,358 @@
+type Stack =
+  | {
+      type: 'object';
+      symbol: string;
+      range: [number, number];
+      id: number;
+    }
+  | {
+      type: 'array';
+      symbol: string;
+      range: [number, number];
+      data: string;
+      id: number;
+    }
+  | {
+      type: 'object_key';
+      symbol: string;
+      range: [number, number];
+      data: string;
+      path: string;
+      id: number;
+      objectId: number;
+    }
+  | {
+      type: 'object_value';
+      symbol: string;
+      range: [number, number];
+      data: string;
+      id: number;
+      keyId: number;
+      objectId: number;
+    }
+  | {
+      type: 'object_value_string';
+      symbol: string;
+      range: [number, number];
+      data: string;
+      path: string;
+      id: number;
+      objectId: number;
+      valueId: number;
+    }
+  | {
+      type: 'object_value_number';
+      symbol: string;
+      range: [number, number];
+      data: string;
+      path: string;
+      id: number;
+      objectId: number;
+      valueId: number;
+    };
+
+type Commitment = {
+  name?: string;
+  path?: string;
+  start: number;
+  end: number;
+};
+
+export function processJSON(str: string): Commitment[] {
+  const json = JSON.parse(str);
+  expect(typeof json === 'object', 'json string must be an object');
+
+  const stack: Stack[] = [];
+  const values: Stack[] = [];
+  const keys: string[] = [];
+  let nonce = 0,
+    keyId = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charAt(i);
+    let last = stack[stack.length - 1];
+
+    if (last?.type === 'object_key') {
+      if (char === '"') {
+        last.range[1] = i;
+        const key = stack.pop();
+        expect(key!.type === 'object_key');
+        if (key?.type === 'object_key') {
+          keys.push(key.data);
+          key.path = keys.join('.');
+          values.push(key);
+          keyId = last.id;
+        }
+      } else {
+        last.data = last.data + char;
+      }
+      continue;
+    }
+
+    if (last?.type === 'object_value_string') {
+      if (char === '"') {
+        last.range[1] = i;
+        values.push(stack.pop()!);
+        const objectValue = stack.pop();
+        expect(
+          objectValue?.type === 'object_value',
+          'expect stack to be object_value',
+        );
+        objectValue!.range[1] = i;
+        values.push(objectValue!);
+        keys.pop();
+      } else {
+        last.data = last.data + char;
+      }
+      continue;
+    }
+
+    if (last?.type === 'array') {
+      if (char === ']') {
+        last.range[1] = i;
+        values.push(stack.pop()!);
+      } else if (char === '[') {
+        stack.push({
+          symbol: '[',
+          type: 'array',
+          id: nonce++,
+          range: [i, -1],
+          data: '',
+        });
+      } else {
+        last.data = last.data + char;
+      }
+      continue;
+    }
+
+    if (last?.type === 'object_value_number') {
+      if (char === ',' || char === '}') {
+        last.range[1] = i - 1;
+        values.push(stack.pop()!);
+        const objectValue = stack.pop();
+        expect(
+          objectValue?.type === 'object_value',
+          'expect stack to be object_value',
+        );
+        objectValue!.range[1] = i - 1;
+        values.push(objectValue!);
+        last = stack[stack.length - 1];
+      } else {
+        last.data = last.data + char;
+        continue;
+      }
+    }
+
+    if (last?.type === 'object_value') {
+      if (char === '}') {
+        last.range[1] = i - 1;
+        values.push(stack.pop()!);
+        const object = stack.pop();
+        expect(object?.type === 'object_value', 'expect stack to be object');
+        object!.range[1] = i;
+        values.push(object!);
+        keys.pop();
+      } else if (char === ',') {
+        last.range[1] = i - 1;
+        values.push(stack.pop()!);
+        keys.pop();
+      } else if (char === '{') {
+        stack.push({
+          symbol: '{',
+          type: 'object',
+          id: nonce++,
+          range: [i, -1],
+        });
+      } else if (char === '[') {
+        stack.push({
+          symbol: '[',
+          type: 'array',
+          id: nonce++,
+          range: [i, -1],
+          data: '',
+        });
+      } else if (char === '"') {
+        stack.push({
+          symbol: '"',
+          type: 'object_value_string',
+          objectId: last.objectId,
+          valueId: last.id,
+          id: nonce++,
+          data: '',
+          range: [i, -1],
+          path: '',
+        });
+      } else if (/^\d$/.test(char)) {
+        stack.push({
+          symbol: '"',
+          type: 'object_value_number',
+          objectId: last.objectId,
+          valueId: last.id,
+          id: nonce++,
+          data: '',
+          range: [i, -1],
+          path: '',
+        });
+      }
+      continue;
+    }
+
+    if (last?.type === 'object') {
+      switch (char) {
+        case '}':
+          last.range[1] = i;
+          values.push(stack.pop()!);
+          continue;
+        case '"':
+          stack.push({
+            symbol: '"',
+            type: 'object_key',
+            objectId: last.id,
+            id: nonce++,
+            data: '',
+            range: [i, -1],
+            path: '',
+          });
+          continue;
+        case ':':
+          stack.push({
+            symbol: ':',
+            type: 'object_value',
+            objectId: last.id,
+            keyId: keyId,
+            id: nonce++,
+            range: [i, -1],
+            data: '',
+          });
+          continue;
+        default:
+          continue;
+      }
+    }
+
+    switch (char) {
+      case '{':
+        stack.push({
+          symbol: '{',
+          type: 'object',
+          id: nonce++,
+          range: [i, -1],
+        });
+        break;
+      case '[':
+        stack.push({
+          symbol: '[',
+          type: 'array',
+          id: nonce++,
+          range: [i, -1],
+          data: '',
+        });
+        break;
+    }
+  }
+
+  expect(!stack.length, 'invalid stack length');
+
+  const commitments: {
+    [key: string]: Commitment;
+  } = {};
+
+  for (const value of values) {
+    if (value.type === 'object_key') {
+      commitments[value.id] = {
+        ...(commitments[value.id] || {}),
+        path: value.path,
+        start: value.range[0],
+      };
+    } else if (value.type === 'object_value') {
+      commitments[value.keyId] = {
+        ...(commitments[value.keyId] || {}),
+        end: value.range[1] + 1,
+      };
+    } else if (value.type === 'object') {
+      commitments[value.id] = {
+        start: value.range[0],
+        end: value.range[1] + 1,
+      };
+    } else if (value.type === 'array') {
+      commitments[value.id] = {
+        start: value.range[0],
+        end: value.range[1] + 1,
+      };
+    }
+  }
+
+  return Object.values(commitments).map(({ path, start, end }) => ({
+    // type,
+    path,
+    start,
+    end,
+    // data: str.slice(start, end),
+  }));
+}
+
+export function processTranscript(transcript: string): Commitment[] {
+  const commitments: Commitment[] = [];
+  let text = '',
+    ptr = -1;
+  for (let i = 0; i < transcript.length; i++) {
+    const char = transcript.charAt(i);
+
+    if (char === '\r') {
+      _processEOL(text, i);
+      continue;
+    }
+
+    if (char === '\n') {
+      text = '';
+      ptr = -1;
+      continue;
+    }
+
+    if (ptr === -1) {
+      ptr = i;
+    }
+
+    text = text + char;
+  }
+
+  _processEOL(text, transcript.length - 1);
+
+  return commitments;
+
+  function _processEOL(txt: string, index: number) {
+    try {
+      if (!txt) return;
+      if (!isNaN(Number(txt))) return;
+
+      const json = JSON.parse(txt);
+
+      if (typeof json === 'object') {
+        const jsonCommits = processJSON(txt);
+        commitments.push(
+          ...jsonCommits.map((commit) => ({
+            ...commit,
+            start: commit.start + ptr,
+            end: commit.end + ptr,
+          })),
+        );
+      } else {
+        commitments.push({
+          start: ptr,
+          end: index,
+        });
+      }
+    } catch (e) {
+      const [name, value] = text.split(':');
+      commitments.push({
+        name: value ? name : '',
+        start: ptr,
+        end: index,
+      });
+    }
+  }
+}
+
+export function expect(cond: any, msg = 'invalid expression') {
+  if (!cond) throw new Error(msg);
+}
