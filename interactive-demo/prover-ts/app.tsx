cb42b4907430d88625ceefa1a1650df9953a31fd
Hendrik Eeckhaut <hendrik@eeckhaut.org>
2024-10-29 17:23:03 +0100

    eslint

--- interactive-demo/prover-ts/app.tsx
+++ interactive-demo/prover-ts/app.tsx
@@ -1,13 +1,11 @@
-import React, { ReactElement, useCallback, useEffect, useState } from 'react';
+import React, { ReactElement, useCallback, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import * as Comlink from 'comlink';
 import { Watch } from 'react-loader-spinner';
-import {
-  Prover as TProver,
-} from 'tlsn-js';
+import { Prover as TProver } from 'tlsn-js';
 import { type Method } from 'tlsn-wasm';
 
-const { init, Prover, NotarizedSession, TlsProof }: any = Comlink.wrap(
+const { init, Prover }: any = Comlink.wrap(
   new Worker(new URL('./worker.ts', import.meta.url)),
 );
 
@@ -18,45 +16,46 @@ root.render(<App />);
 
 function App(): ReactElement {
   const [processing, setProcessing] = useState(false);
-  const [result, setResult] = useState<String | null>(null);
+  const [result, setResult] = useState<string | null>(null);
 
   const onClick = useCallback(async () => {
     setProcessing(true);
 
-    let url = "https://swapi.dev/api/people/1";
-    let method: Method = 'GET';
-    let headers = {
-      'secret': "TLSNotary's private key",
+    const url = 'https://swapi.dev/api/people/1';
+    const method: Method = 'GET';
+    const headers = {
+      secret: "TLSNotary's private key",
       'Content-Type': 'application/json',
     };
-    let body = {};
+    const body = {};
     // let websocketProxyUrl = 'wss://notary.pse.dev/proxy';
-    let websocketProxyUrl = 'ws://localhost:55688';
-    let verifierProxyUrl = 'ws://localhost:9816/verify';
+    const websocketProxyUrl = 'ws://localhost:55688';
+    const verifierProxyUrl = 'ws://localhost:9816/verify';
     const hostname = new URL(url).hostname;
 
     console.time('setup');
 
     await init({ loggingLevel: 'Info' });
 
-    console.log("Setting up Prover for", hostname)
-    const prover = await new Prover({ serverDns: hostname }) as TProver;
-    console.log("Setting up Prover: 1/2")
+    console.log('Setting up Prover for', hostname);
+    const prover = (await new Prover({ serverDns: hostname })) as TProver;
+    console.log('Setting up Prover: 1/2');
     await prover.setup(verifierProxyUrl);
-    console.log("Setting up Prover: done")
+    console.log('Setting up Prover: done');
 
     console.timeEnd('setup');
 
     console.time('request');
-    console.log("Sending request to proxy")
+    console.log('Sending request to proxy');
     const resp = await prover.sendRequest(
-      `${websocketProxyUrl}?token=${hostname}`, { url, method, headers, body, }
+      `${websocketProxyUrl}?token=${hostname}`,
+      { url, method, headers, body },
     );
-    console.log("Response:", resp);
+    console.log('Response:', resp);
 
-    console.log("Wait for transcript")
+    console.log('Wait for transcript');
     const transcript = await prover.transcript();
-    console.log("Transcript:", transcript);
+    console.log('Transcript:', transcript);
 
     console.timeEnd('request');
 
@@ -81,15 +80,18 @@ function App(): ReactElement {
         ...transcript.ranges.recv.lineBreaks,
       ],
     };
-    console.log("Start reveal:", reveal);
+    console.log('Start reveal:', reveal);
     await prover.reveal(reveal);
     console.timeEnd('reveal');
 
-    console.log("Ready");
+    console.log('Ready');
 
-    console.log("Unredacted data:", { sent: transcript.sent, received: transcript.recv })
+    console.log('Unredacted data:', {
+      sent: transcript.sent,
+      received: transcript.recv,
+    });
 
-    setResult("Unredacted data successfully revealed to Verifier.");
+    setResult('Unredacted data successfully revealed to Verifier.');
 
     setProcessing(false);
   }, [setResult, setProcessing]);
@@ -98,7 +100,9 @@ function App(): ReactElement {
     <div>
       <h1>TLSNotary interactive prover demo</h1>
       <div>
-        Before clicking the start button, make sure the <i>interactive verifier</i> and the <i>web socket proxy</i> are running. Check the README for the details.
+        Before clicking the start button, make sure the{' '}
+        <i>interactive verifier</i> and the <i>web socket proxy</i> are running.
+        Check the README for the details.
       </div>
 
       <br />
