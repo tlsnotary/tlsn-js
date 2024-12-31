35019a279cdd34d243cedf7aa41f76bb3fc98b30
Artur Pokusin <gh@pokusin.com>
2024-12-31 00:41:57 -0800

    fix: sentence casing

--- demo/react-ts-webpack/src/components/verify-nft.tsx
+++ demo/react-ts-webpack/src/components/verify-nft.tsx
@@ -119,7 +119,7 @@ export function VerifyNFT(): ReactElement {
                     Verifying...
                   </>
                 ) : (
-                  <>Verify Attestation</>
+                  <>Verify attestation</>
                 )}
               </Button>
             </div>
